2d8c47c42805fe430e809cee5b723521c4f73809
Andrey Listochkin <listochkin@outlook.com>
2016-01-05 14:00:09 +0200

    basic JSON resource

--- src/crud.js
+++ src/crud.js
@@ -2,4 +2,23 @@
 
 const jsonApi = require('jsonapi-server');
 
+jsonApi.define({
+  resource: 'messages',
+  handlers: new jsonApi.MemoryHandler(),
+  attributes: {
+    text: jsonApi.Joi.string().required(),
+    createDate: jsonApi.Joi.date().iso().default(Date.now, 'time of creation')
+  },
+
+  examples: [{
+    id: '11',
+    type: 'messages',
+    text: 'Hello World'
+  }, {
+    id: '12',
+    type: 'messages',
+    text: 'Second Comment'
+  }]
+});
+
 module.exports = jsonApi;
--- test/rest-tests.js
+++ test/rest-tests.js
@@ -9,11 +9,19 @@ const co = require('co');
 const crud = require('../src/crud');
 
 describe('CRUD tests with jsonapi-server', () => {
+  const port = process.env.PORT || 3000;
+  const rootUrl = 'http://localhost:' + port;
+
   it('should load messages', co.wrap(function* () {
-    assert.fail('not implemented');
+    const res = yield fetch(rootUrl + '/messages');
+    assert(res.ok, 'no endpoint found' + res.status);
+
+    const json = yield res.json();
+    assert.equal(json.data[0].id, '11', '');
   }));
 
   before(() => {
+    crud.setConfig({ port });
     crud.start();
   });
 
