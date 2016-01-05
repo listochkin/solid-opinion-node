9f28768602bfed11af401a3ea87c813e23aa9a47
Andrey Listochkin <listochkin@outlook.com>
2016-01-05 11:10:53 +0200

    async test using promises

--- test/start-testing.js
+++ test/start-testing.js
@@ -3,8 +3,19 @@
 const chai = require('chai');
 const assert = chai.assert;
 
+const fetch = require('node-fetch');
+
 describe('some tests', () => {
   it('should pass', () => {
     assert(1 == '1');
+  });
+
+  it('should run async tests', () => {
+    return fetch('https://google.com').then(res => {
+      assert(res.ok);
+      return res.text();
+    }).then(text => {
+      assert.equal(text.substr(0, 5), '<!doc')
+    });
   });
 });
