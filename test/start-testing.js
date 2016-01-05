6897fb2ce32fcc42839e024a8e8e9ea0f79dff36
Andrey Listochkin <listochkin@outlook.com>
2016-01-05 11:26:14 +0200

    async tests with promises and generators

--- test/start-testing.js
+++ test/start-testing.js
@@ -4,18 +4,18 @@ const chai = require('chai');
 const assert = chai.assert;
 
 const fetch = require('node-fetch');
+const co = require('co');
 
 describe('some tests', () => {
   it('should pass', () => {
     assert(1 == '1');
   });
-
-  it('should run async tests', () => {
-    return fetch('https://google.com').then(res => {
-      assert(res.ok);
-      return res.text();
-    }).then(text => {
-      assert.equal(text.substr(0, 5), '<!doc')
-    });
-  });
+                              // async function
+  it('should run async tests', co.wrap(function* () {
+    const res = yield fetch('https://google.com');
+    assert(res.ok);
+              // await
+    const text = yield res.text();
+    assert.equal(text.substr(0, 5), '<!doc')
+  }));
 });
