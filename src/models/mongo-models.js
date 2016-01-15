f6e2f6b412832d08e8716d30a94a60db43871236
Andrey Listochkin <listochkin@outlook.com>
2016-01-15 13:18:14 +0200

    fix mongoose errors on test restarts

--- src/models/mongo-models.js
+++ src/models/mongo-models.js
@@ -3,42 +3,49 @@
 const mongoose = require('mongoose'),
   Schema = mongoose.Schema;
 
-//
-// Schemas definitions
-//
-const OAuthAccessTokensSchema = new Schema({
-  accessToken: { type: String },
-  clientId: { type: String },
-  userId: { type: String },
-  expires: { type: Date }
-});
+let modelsAreNotDefinedYet = false;
 
-const OAuthRefreshTokensSchema = new Schema({
-  refreshToken: { type: String },
-  clientId: { type: String },
-  userId: { type: String },
-  expires: { type: Date }
-});
+try {
+  mongoose.model('OAuthAccessTokens');
+} catch (o_O) {
+  modelsAreNotDefinedYet = true;
+}
 
-const OAuthClientsSchema = new Schema({
-  clientId: { type: String },
-  clientSecret: { type: String },
-  grantTypes: [ {type: String} ],
-  redirectUri: { type: String }
-});
+if (modelsAreNotDefinedYet) {
+  const OAuthAccessTokensSchema = new Schema({
+    accessToken: { type: String },
+    clientId: { type: String },
+    userId: { type: String },
+    expires: { type: Date }
+  });
 
-const UsersSchema = new Schema({
-  username: { type: String },
-  passwordHash: { type: String },
-  firstname: { type: String },
-  lastname: { type: String },
-  email: { type: String, default: '' }
-});
+  const OAuthRefreshTokensSchema = new Schema({
+    refreshToken: { type: String },
+    clientId: { type: String },
+    userId: { type: String },
+    expires: { type: Date }
+  });
 
-mongoose.model('OAuthAccessTokens', OAuthAccessTokensSchema);
-mongoose.model('OAuthRefreshTokens', OAuthRefreshTokensSchema);
-mongoose.model('OAuthClients', OAuthClientsSchema);
-mongoose.model('Users', UsersSchema);
+  const OAuthClientsSchema = new Schema({
+    clientId: { type: String },
+    clientSecret: { type: String },
+    grantTypes: [ {type: String} ],
+    redirectUri: { type: String }
+  });
+
+  const UsersSchema = new Schema({
+    username: { type: String },
+    passwordHash: { type: String },
+    firstname: { type: String },
+    lastname: { type: String },
+    email: { type: String, default: '' }
+  });
+
+  mongoose.model('OAuthAccessTokens', OAuthAccessTokensSchema);
+  mongoose.model('OAuthRefreshTokens', OAuthRefreshTokensSchema);
+  mongoose.model('OAuthClients', OAuthClientsSchema);
+  mongoose.model('Users', UsersSchema);
+}
 
 module.exports = {
   OAuthAccessTokensModel: mongoose.model('OAuthAccessTokens'),
