04160ae2e5ecf3d6aad9e7ddf668a5a965adda38
Andrey Listochkin <listochkin@outlook.com>
2016-01-22 13:18:46 +0200

    use cryptic to store Mongo credentials

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM node:5.5-onbuild
+EXPOSE 3000
+CMD npm start
--- src/pm2-launcher.js
+++ src/pm2-launcher.js
@@ -1,47 +1,52 @@
 const pm2 = require('pm2');
+const cryptex = require('cryptex');
 
 const instances = process.env.WEB_CONCURRENCY || -1; // Set by Heroku or -1 to scale to max cpu core -1
 const maxMemory = process.env.WEB_MEMORY || 512;    // " " "
 const mongodbUrl = process.env.MONGODB_URL || 'mongodb://localhost/test';
 
 pm2.connect(function() {
-  pm2.start([{
-    script    : './src/server-launcher.js',
-    name      : 'solid-opinion-auth',
-    exec_mode : 'cluster',
-    node_args : '--abort_on_uncaught_exception --perf_basic_prof',
-    instances : 1,
-    max_memory_restart : maxMemory + 'M',
-    env: {
-      'APP_NAME': './auth',
-      'PORT': 3000,
-      'MONGODB_URL' : mongodbUrl
-    },
-  }, {
-    script: './src/server-launcher.js',
-    name: 'solid-opinion-crud',
-    exec_mode : 'cluster',
-    node_args : '--abort_on_uncaught_exception --perf_basic_prof',
-    instances : instances,
-    max_memory_restart : maxMemory + 'M',
-    env: {
-      'APP_NAME': './crud',
-      'PORT': 5000
-    },
-  }], function(err) {
-    if (err) return console.error('Error while launching applications', err.stack || err);
-    console.log('PM2 and application has been succesfully started');
+  // https://technologyadvice.github.io/lock-up-your-customer-accounts-give-away-the-key/
+  cryptex.getSecrets(['mongodbUrl']).then(secrets => {
 
-    // Display logs in standard output
-    pm2.launchBus(function(err, bus) {
-      console.log('[PM2] Log streaming started');
+    pm2.start([{
+      script    : './src/server-launcher.js',
+      name      : 'solid-opinion-auth',
+      exec_mode : 'cluster',
+      node_args : '--abort_on_uncaught_exception --perf_basic_prof',
+      instances : 1,
+      max_memory_restart : maxMemory + 'M',
+      env: {
+        'APP_NAME': './auth',
+        'PORT': 3000,
+        'MONGODB_URL' : secrets.mongodbUrl || mongodbUrl
+      },
+    }, {
+      script: './src/server-launcher.js',
+      name: 'solid-opinion-crud',
+      exec_mode : 'cluster',
+      node_args : '--abort_on_uncaught_exception --perf_basic_prof',
+      instances : instances,
+      max_memory_restart : maxMemory + 'M',
+      env: {
+        'APP_NAME': './crud',
+        'PORT': 5000
+      },
+    }], function(err) {
+      if (err) return console.error('Error while launching applications', err.stack || err);
+      console.log('PM2 and application has been succesfully started');
 
-      bus.on('log:out', function(packet) {
-       console.log('[App:%s] %s', packet.process.name, packet.data);
-      });
+      // Display logs in standard output
+      pm2.launchBus(function(err, bus) {
+        console.log('[PM2] Log streaming started');
 
-      bus.on('log:err', function(packet) {
-        console.error('[App:%s][Err] %s', packet.process.name, packet.data);
+        bus.on('log:out', function(packet) {
+         console.log('[App:%s] %s', packet.process.name, packet.data);
+        });
+
+        bus.on('log:err', function(packet) {
+          console.error('[App:%s][Err] %s', packet.process.name, packet.data);
+        });
       });
     });
 
